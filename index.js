6e6a85893ce64aa5674914456f5c7c9c19501083
Jhanae <jhanaechisholm12@gmail.com>
2021-05-18 09:38:43 -0400

    commit tree 3a8ae39eeea0ac561ef0173d046fe84b152e9aba

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'Jay';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+//excitedWelcomeMessage.toUpperCase();
+let firstInitial = currentUser[0];
+let restOfName = currentUser.slice(1);
+let shortGreeting = 'Welcome, ' + firstInitial + '!';
+
